346325d2a6d35fed69f37d5883c4ee96ee733306
Olga <Olga1987h@gmail.com>
2020-04-04 01:12:31 -0500

    added failure

--- caller.groovy
+++ caller.groovy
@@ -4,16 +4,20 @@ node {
 } 
     stage("Stage2"){ 
         echo "hello" 
+        // Calls another job called "Timestamp"
         build 'Timestamp'
 } 
     stage("Stage3"){ 
         echo "Hello" 
+        // Calls another job called "Script"
         build "Script"
 } 
     stage("Stage4"){ 
         build 'Template2' 
+        // Calls another job called "Script"
 } 
     stage("Stage5"){ 
         echo "hello" 
+        build 'nonexisting'
     } 
 } 
