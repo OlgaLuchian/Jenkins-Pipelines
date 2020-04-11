ddf3a6547ab8e97a262f67fdb84c1e31ecd46ea5
Olga <Olga1987h@gmail.com>
2020-04-11 16:49:13 -0500

    added

--- packer_builder.groovy
+++ packer_builder.groovy
@@ -8,25 +8,27 @@ node {
                 'elk',  
                 'gitlab', 
                 'source_ami_filter'],  
-                description: 'What would you like to build? ',  
-                name: 'TOOL'),  
+        description: 'What would you like to build? ',  
+        name: 'TOOL'),  
                 choice(choices:  
+                [
                 'us-east-1',  
                 'us-east-2',  
                 'us-west-1',  
                 'us-west-2',  
                 'eu-west-1',  
                 'eu-west-2',  
-                'eu-central-1'),  
-                description: 'Where would you like to build? ',  
-                name: 'REGION')])]) 
+                'eu-central-1'],  
+            description: 'Where would you like to build? ',  
+            name: 'REGION')])]) 
                 stage("Checkout SCM"){ 
                     timestamps { 
                         ws { 
                             echo "Slack" 
                             checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/packer.git']]]) 
-                            } 
-                            } 
+                        } 
+                    }
+                }     
                         
     stage("Run Packer"){ 
         timestamps { 
