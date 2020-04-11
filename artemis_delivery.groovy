0e9a96b5d1793c7c216a4ef6f2a7712f7e03af70
Olga <Olga1987h@gmail.com>
2020-04-11 00:03:17 -0500

    updated

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -7,15 +7,23 @@ node {
 				'0.2', 
 				'0.3', 
 				'0.4', 
-				'0.5'], 
+				'0.5',
+                '0.6',
+                '0.7',
+                '0.8',
+                '0.9',
+                '10',
+            ], 
 	    description: 'Which version of the app should I deploy? ', 
-	    name: 'Version'),
+	    name: 'Version')])]),
         stage("Stage1"){
 		timestamps {
-			ws {
-                checkout([$class: 'GitSCM', branches: [[name: 'dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		
-        }	
+			    ws {
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		
+            }	
+        }
     }
+    
     stage("Get Credentials"){
 		timestamps {
 			ws{
@@ -34,5 +42,70 @@ node {
             } 
         } 
     }
-}    
+    stage("Tag Image"){ 
+        timestamps { 
+            ws { 
+                sh ''' 
+                docker tag artemis:${Version} 777042527031.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
+                ''' 
+            } 
+        } 
+    } 
+    stage("Push Image"){ 
+        timestamps { 
+            ws { 
+                sh ''' 
+                docker push 777042527031.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
+                ''' 
+            } 
+        } 
+    } 
+    stage("Send slack notifications"){ 
+        timestamps { 
+            ws { 
+                echo "Slack" 
+                //slackSend color: '#BADA55', message: 'Hello, World!' 
+            } 
+        } 
+    } 
+    stage("Authenticate"){ 
+        timestamps { 
+            ws { 
+                sh ''' 
+                ssh centos@dev1.olgaandolga.com $(aws ecr get-login --no-include-email --region us-east-1) 
+                ''' 
+            } 
+        } 
+    } 
+    stage("Clean Up"){ 
+        timestamps { 
+            ws { 
+                try { 
+                    sh ''' 
+                    #!/bin/bash 
+                    IMAGES=$(ssh centos@dev1.olgaandolga.com docker ps -aq)  
+                    for i in \$IMAGES; do 
+                       ssh centos@dev1.olgaandolga.com docker stop \$i 
+                       ssh centos@dev1.olgaandolga.com docker rm \$i 
+                    done  
+                    ''' 
+                    } catch(e) { 
+                        println("Script failed with error: ${e}") 
+                } 
+            } 
+        } 
+    } 
+
+    stage("Run Container"){ 
+        timestamps { 
+            ws { 
+                sh ''' 
+                ssh centos@dev1.olgaandolga.com docker run -dti -p 5001:5000 777042527031.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
+                ''' 
+            } 
+        } 
+    } 
+} 
+
+ 
     
